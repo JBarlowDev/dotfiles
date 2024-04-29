5b90fc26e824aca774030c35b880980aecc45695
JBarlowDev <dev@jbarlow.uk>
2024-04-29 23:59:12 +0100

    Adding initial hyprland config

---
+++ .config/hypr/hyprpaper.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+monitor=`hyprctl monitors | grep Monitor | awk '{print $2}'`
+
+initial="$(find -L ~/wallpaper -type f | shuf -n 1)"
+
+#hyprctl hyprpaper preload "$initial"
+#hyprctl hyprpaper wallpaper "$monitor,$initial"
+
+while true; do
+  wallpaper="$(find -L ~/wallpaper -type f | shuf -n 1)"
+
+  hyprctl hyprpaper preload "$wallpaper"
+  hyprctl hyprpaper wallpaper "$monitor,$wallpaper"
+
+  sleep 60
+
+  hyprctl hyprpaper unload "$wallpaper"
+done
+
