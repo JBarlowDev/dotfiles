2a13961c43922f320abac177a8d27d8e59cee11e
JBarlowDev <dev@jbarlow.uk>
2025-01-01 18:57:36 +0000

    Fish using fzf

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,12 +2,20 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+fzf --fish | source
 
 abbr g 'git status'
 abbr gd 'git diff'
 
 abbr lg 'lazygit'
-
+abbr zelli 'zellij --layout compact options --theme tokyo-night --pane-frames false'
 
 set fish_greeting
 neofetch
+
+# No longer using zellij
+# if set -q ZELLIJ
+# else
+#   zellij --layout compact options --theme tokyo-night --pane-frames false
+# end
+
