25cfc3aa20d6acff3c6533dd4fb842798da3f064
JBarlowDev <dev@jbarlow.uk>
2025-01-01 18:57:59 +0000

    Updating some hyperland keys

--- .config/hypr/hyprpaper.sh
+++ .config/hypr/hyprpaper.sh
@@ -4,8 +4,8 @@ monitor=`hyprctl monitors | grep Monitor | awk '{print $2}'`
 
 initial="$(find -L ~/wallpaper -type f | shuf -n 1)"
 
-#hyprctl hyprpaper preload "$initial"
-#hyprctl hyprpaper wallpaper "$monitor,$initial"
+hyprctl hyprpaper preload "$initial"
+hyprctl hyprpaper wallpaper "$monitor,$initial"
 
 while true; do
   wallpaper="$(find -L ~/wallpaper -type f | shuf -n 1)"
