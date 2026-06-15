96c1b4baee745a1ec8bca3c638d35bcffe921cc2
JamesABarlow <jamesbarlow31@gmail.com>
2026-06-15 11:33:27 +0100

    [nvim] Add pyrite, update lsp keymap, start treesitter for python

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -1,35 +1,33 @@
 local opts = { noremap = true, silent = true }
 local capabilities = vim.tbl_deep_extend(
-  "force",
+  'force',
   {},
   vim.lsp.protocol.make_client_capabilities()
- -- require("blink.cmp").get_lsp_capabilities()
- -- TODO: Add back in blink
+  -- TODO: add back , require('blink.cmp').get_lsp_capabilities()
 )
+
 
 vim.lsp.config('*', {
   capabilities = capabilities,
-  on_attach = function(_, bufnr)
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', {})
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gS', '<cmd>lua vim.lsp.buf.declaration()<CR>', {})
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', {})
+})
+    vim.api.nvim_set_keymap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', {})
+    vim.api.nvim_set_keymap('n', 'gS', '<cmd>lua vim.lsp.buf.declaration()<CR>', {})
+    vim.api.nvim_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', {})
     -- vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gs', '<cmd>lua vim.lsp.buf.definition()<CR>', {})
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gs', '<cmd>lua require("snacks").picker.lsp_definitions()<CR>', {})
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', {})
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gI', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', {})
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', {})
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', {})
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space><space>a', '<cmd>lua vim.lsp.buf.code_action()<CR>', {})
+    vim.api.nvim_set_keymap('n', 'gs', '<cmd>lua require("snacks").picker.lsp_definitions()<CR>', {})
+    vim.api.nvim_set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', {})
+    vim.api.nvim_set_keymap('n', 'gI', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+    vim.api.nvim_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', {})
+    vim.api.nvim_set_keymap('n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', {})
+    vim.api.nvim_set_keymap('n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', {})
+    vim.api.nvim_set_keymap('n', '<space><space>a', '<cmd>lua vim.lsp.buf.code_action()<CR>', {})
     -- vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space><space>a', '<cmd>FzfLua lsp_code_actions<CR>', {})
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gR', '<cmd>lua require("fzf-lua").lsp_references({ignore_current_line = true})<CR>', {})
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gr', '<cmd>lua require("snacks").picker.lsp_references()<CR>', {})
+    vim.api.nvim_set_keymap('n', 'gR', '<cmd>lua require("fzf-lua").lsp_references({ignore_current_line = true})<CR>', {})
+    vim.api.nvim_set_keymap('n', 'gr', '<cmd>lua require("snacks").picker.lsp_references()<CR>', {})
     -- { "<Leader>gr", function() require('fzf-lua').lsp_references({ ignore_current_line = true }) end, desc = "Fzf lsp references" },
     -- Using conform for formatting over lsp
     -- vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space><space>f', '<cmd>lua vim.lsp.buf.format()<CR>', {})
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>cd', '<cmd>lua vim.diagnostic.open_float()<CR>', {})
-  end
-})
+    vim.api.nvim_set_keymap('n', '<space>cd', '<cmd>lua vim.diagnostic.open_float()<CR>', {})
 
 -- Can extend on attach for specific lsp's like below
 -- vim.lsp.config('vtsls', {
@@ -41,4 +39,5 @@ vim.lsp.config('*', {
 --
 
 -- vim.lsp.enable('vtsls')
+vim.lsp.enable('pyright')
 vim.lsp.enable('lua_ls')
--- .config/nvim/plugin/lsp.lua
+++ .config/nvim/plugin/lsp.lua
@@ -1,7 +1,30 @@
 vim.pack.add({
   'https://github.com/williamboman/mason.nvim',
-  'https://github.com/neovim/nvim-lspconfig'
+  'https://github.com/neovim/nvim-lspconfig',
+  'https://github.com/nvim-treesitter/nvim-treesitter',
 })
 
 require('mason').setup()
 
+-- Start treesitter on buffer open
+-- Am I overthinking this, any good reason not to try on every buffer open?
+
+-- vim.api.nvim_create_autocmd('FileType', {
+--   pattern = { '*' },
+--   callback = function()
+--     local filetype = vim.bo.filetype
+--     if filetype and filetype ~= '' then
+--       local success = pcall(function()
+--         vim.treesitter.start()
+--       end)
+--       if not success then
+--         return
+--       end
+--     end
+--   end,
+-- })
+
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = { 'python' },
+  callback = function() vim.treesitter.start() end,
+})
