aa98d181491abac533c8aa9fc242fa3397c35e92
JamesABarlow <jamesbarlow31@gmail.com>
2026-06-23 11:29:50 +0100

    [niri] Add mako as startup spawn

--- .config/niri/wallpaper.sh
+++ .config/niri/wallpaper.sh
@@ -1,14 +1,8 @@
 #!/bin/sh
 
-# initial="$(find -L ~/Wallpapers -maxdepth 1 -type f | shuf -n 1)"
-#
-# swww img "$initial" 
-
 awww img $HOME/Wallpapers/wallhaven-1jod8v.png
 
 while true; do 
-
-
   if [[ $1 -eq 1 ]]; then
     wallpaper="$(find -L ~/Wallpapers/extra/ -maxdepth 1 -type f | shuf -n 1)"
   else
