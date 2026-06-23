250c5fb7a03b0edee001eace5a395926083bc86a
JamesABarlow <jamesbarlow31@gmail.com>
2026-06-23 10:38:33 +0100

    [fish] Small updates / using fastfetch over neofetch

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,7 +2,9 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-fzf --fish | source
+if type -q fzf
+  fzf --fish | source
+end
 
 abbr g 'git status'
 abbr gd 'git diff'
@@ -11,7 +13,10 @@ abbr lg 'lazygit'
 abbr zelli 'zellij --layout compact options --theme tokyo-night --pane-frames false'
 
 set fish_greeting
-neofetch
+
+if type -q fastfetch
+  fastfetch
+end
 
 # No longer using zellij
 # if set -q ZELLIJ
